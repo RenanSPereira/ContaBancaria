2e6e84b8bc3276bcf67288834411ce6907cac14c
Renan Santos <renanspereira.dev@gmail.com>
2023-02-24 20:57:16 -0300

    movendo logica de movemetacoes para o extrato

--- src/ContaBancaria.Domain/Conta.cs
+++ src/ContaBancaria.Domain/Conta.cs
@@ -4,33 +4,12 @@ public class Conta
 {
     public int Numero { get; private set; }
     public TipoConta TipoConta { get; private set; }
-    public Saldo Saldo { get; private set; }
-    private List<Movimentacao> _movimentacoes;
-    public IReadOnlyCollection<Movimentacao> Movimentacoes { get; private set; }
+    public Extrato Extrato { get; private set; }
 
-    private Conta()
-    {
-        _movimentacoes = new List<Movimentacao>();
-    }
-
-    public Conta(TipoConta tipoConta, int numero, Saldo saldo) : this()
+    public Conta(TipoConta tipoConta, int numero, Extrato extrato)
     {
         Numero = numero;
         TipoConta = tipoConta;
-        Saldo = saldo;
-    }
-
-    public void AdicionarMovimentacao(Movimentacao movimentacao)
-    {
-        _movimentacoes.Add(movimentacao);
-    }
-
-    public void RecalcularSaldo()
-    {
-        var movimentacoesDiaAtual = _movimentacoes
-            .Where(m => m.DataCadastro.Date == DateTime.Now.Date)
-            .ToList();
-
-        Saldo.Recalcular(movimentacoesDiaAtual);
+        Extrato = extrato;
     }
 }
