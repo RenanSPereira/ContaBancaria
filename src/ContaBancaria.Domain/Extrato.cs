3df0df9b8af0a59e4f173a95e1e2a72d90fe12dc
Renan Santos <renanspereira.dev@gmail.com>
2023-02-24 20:56:58 -0300

    criando objeto extrato

---
+++ src/ContaBancaria.Domain/Extrato.cs
@@ -0,0 +1,36 @@
+namespace ContaBancaria.Domain;
+
+public class Extrato
+{
+    public Saldo SaldoInicial { get; private set; }
+    public Saldo SaldoAtual { get; private set; }
+    private List<Movimentacao> _movimentacoes;
+    public IReadOnlyCollection<Movimentacao> Movimentacoes => _movimentacoes;
+
+    private Extrato()
+    {
+        _movimentacoes = new List<Movimentacao>();
+    }
+
+    public Extrato(Saldo saldo, Saldo saldoAtual) : this()
+    {
+        SaldoInicial = saldo;
+        SaldoAtual = saldoAtual;
+    }
+
+    public void AdicionarMovimentacao(Movimentacao movimentacao)
+    {
+        _movimentacoes.Add(movimentacao);
+    }
+
+    public void RecalcularSaldo()
+    {
+        var movimentacoesDiaAtual = _movimentacoes
+            .Where(m => m.DataCadastro.Date == DateTime.Now.Date)
+            .ToList();
+
+        var resultado = SaldoInicial.Valor + movimentacoesDiaAtual.Sum(m => m.Moeda.Valor);
+
+        SaldoAtual.AtribuirValor(resultado);
+    }
+}
