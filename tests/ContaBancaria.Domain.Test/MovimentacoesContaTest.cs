749df07a2702bbb4ddef8e943fa6d676ae34496d
Renan Santos <renanspereira.dev@gmail.com>
2023-02-24 21:16:03 -0300

    refatorando testes para inutilizar o objeto moeda

--- tests/ContaBancaria.Domain.Test/MovimentacoesContaTest.cs
+++ tests/ContaBancaria.Domain.Test/MovimentacoesContaTest.cs
@@ -7,15 +7,15 @@ public class MovimentacoesContaTest
     {
         var cliente = new Cliente("Cliente Teste");
 
-        cliente.AdicionarConta(new Conta(TipoConta.Corrente, 100, new Saldo(new Moeda(0.0m), new Moeda(0.0m))));
+        cliente.AdicionarConta(new Conta(TipoConta.Corrente, 100, new Extrato(new Saldo(0.0m), new Saldo(0.0m))));
 
         var conta = cliente.ObterConta(TipoConta.Corrente);
 
-        conta?.AdicionarMovimentacao(new Movimentacao(new Moeda(5.0m), TipoMovimentacao.Credito));
+        conta?.Extrato.AdicionarMovimentacao(new Movimentacao(5.0m, TipoMovimentacao.Credito));
 
-        conta?.RecalcularSaldo();
+        conta?.Extrato.RecalcularSaldo();
 
-        Assert.Equal("R$ 5,00", conta?.Saldo.Atual.ToString());
+        Assert.Equal("R$ 5,00", conta?.Extrato.SaldoAtual.ToString());
     }
 
     [Fact]
@@ -23,17 +23,17 @@ public class MovimentacoesContaTest
     {
         var cliente = new Cliente("Cliente Teste");
 
-        cliente.AdicionarConta(new Conta(TipoConta.Corrente, 100, new Saldo(new Moeda(0.0m), new Moeda(0.0m))));
+        cliente.AdicionarConta(new Conta(TipoConta.Corrente, 100, new Extrato(new Saldo(0.0m), new Saldo(0.0m))));
 
         var conta = cliente.ObterConta(TipoConta.Corrente);
 
-        conta?.AdicionarMovimentacao(new Movimentacao(new Moeda(5.0m), TipoMovimentacao.Credito));
-        
-        conta?.AdicionarMovimentacao(new Movimentacao(new Moeda(1.5m), TipoMovimentacao.Debito));
+        conta?.Extrato.AdicionarMovimentacao(new Movimentacao(5.0m, TipoMovimentacao.Credito));
 
-        conta?.RecalcularSaldo();
+        conta?.Extrato.AdicionarMovimentacao(new Movimentacao(1.5m, TipoMovimentacao.Debito));
 
-        Assert.Equal("R$ 3,50", conta?.Saldo.Atual.ToString());
+        conta?.Extrato.RecalcularSaldo();
+
+        Assert.Equal("R$ 3,50", conta?.Extrato.SaldoAtual.ToString());
     }
 
     [Fact]
@@ -41,16 +41,16 @@ public class MovimentacoesContaTest
     {
         var cliente = new Cliente("Cliente Teste");
 
-        cliente.AdicionarConta(new Conta(TipoConta.Corrente, 100, new Saldo(new Moeda(2000m), new Moeda(0.0m))));
+        cliente.AdicionarConta(new Conta(TipoConta.Corrente, 100, new Extrato(new Saldo(2000.0m), new Saldo(0.0m))));
 
         var conta = cliente.ObterConta(TipoConta.Corrente);
 
-        conta?.AdicionarMovimentacao(new Movimentacao(new Moeda(5.0m), TipoMovimentacao.Credito));
+        conta?.Extrato.AdicionarMovimentacao(new Movimentacao(5.0m, TipoMovimentacao.Credito));
 
-        conta?.AdicionarMovimentacao(new Movimentacao(new Moeda(1.5m), TipoMovimentacao.Debito));
+        conta?.Extrato.AdicionarMovimentacao(new Movimentacao(1.5m, TipoMovimentacao.Debito));
 
-        conta?.RecalcularSaldo();
+        conta?.Extrato.RecalcularSaldo();
 
-        Assert.Equal("R$ 2.003,50", conta?.Saldo.Atual.ToString());
+        Assert.Equal("R$ 2.003,50", conta?.Extrato.SaldoAtual.ToString());
     }
 }
